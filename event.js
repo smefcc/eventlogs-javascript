4275e400c5626914cbc15a6ef7e576485b676755
justin <36273403+smefpw@users.noreply.github.com>
2020-04-24 19:45:25 +0100

    Update event.js

--- event.js
+++ event.js
@@ -1,7 +1,7 @@
  /*
 MIT License
 
-Copyright (c) 2018 smef.pw (smef.cc)
+Copyright (c) 2018 - 2020 smef.pw
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
