80df86a61efc723c0453ba28b020ae6441aae0c9
justin <36273403+smefpw@users.noreply.github.com>
2020-05-06 02:06:09 +0100

    fix indentation, update script and avoid deprecated functions
    (still shitty)

--- event.js
+++ event.js
@@ -1,4 +1,4 @@
- /*
+/*
 MIT License
 
 Copyright (c) 2018 - 2020 smef.pw
@@ -21,41 +21,42 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
- 
-var i=0
 
-function checkConsole(){
+let i = 0;
+
+function checkConsole() {
     if (i === 10) {
-	document.getElementById('smefpw2').innerHTML = "<font face=\"verdana\"><font color=\"#009EBD\">[smef]</font> console reset<br></font>";
-	i=1;
-	}
+        document.getElementById('smefpw2').innerHTML = "<span style=\"font-family: verdana, sans-serif;\"><span style=\"color: #009EBD;\">[smef]</span> console reset<br></span>";
+        i = 1;
+    }
 }
 
 // disable rightclick
-document.addEventListener("contextmenu", function(disablemouse) {
- disablemouse.preventDefault();
-document.getElementById('smefpw2').innerHTML += "<font face=\"verdana\"><font color=\"#009EBD\">[smef]</font> context menu is disabled</font><br>";
-i += 1;
-checkConsole();
+document.addEventListener("contextmenu", function (disablemouse) {
+    disablemouse.preventDefault();
+    document.getElementById('smefpw2').innerHTML += "<span style=\"font-family: verdana, sans-serif;\"><span style=\"color: #009EBD;\">[smef]</span> context menu is disabled</font><br>";
+    i += 1;
+    checkConsole();
 }, false);
 // disable rightclick
 
 // urls functions
 function example() {
-	document.getElementById('smefpw2').innerHTML += "<font face=\"verdana\"><font color=\"#009EBD\">[smef]</font> example link was clicked</font><br>";
-	i += 1;
-	checkConsole();
+    document.getElementById('smefpw2').innerHTML += "<span style=\"font-family: verdana, sans-serif;\"><span style=\"color: #009EBD;\">[smef]</span> example link was clicked</font><br>";
+    i++;
+    checkConsole();
 }
+
 // urls functions
 
 // site checker
 function checkURL() {
-var currentLocation = window.location.href;
-	if (currentLocation == "https://example.org") {
-		document.getElementById('smefpw').innerHTML += "<br><font face=\"verdana\"><font color=\"#009EBD\">[smef]</font> official site<br></font>";
-}	
-else {
-		document.getElementById('smefpw').innerHTML += "<br><font face=\"verdana\"><font color=\"#009EBD\">[smef]</font> unverified site current site " + window.location.href + " differs from official site smef.pw<br></font>";
-	}
+    const currentLocation = window.location.href;
+    if (currentLocation === "https://example.org") {
+        document.getElementById('smefpw').innerHTML += "<br><span style=\"font-family: verdana, sans-serif;\"><span style=\"color: #009EBD;\">[smef]</span> official site<br></font>";
+    } else {
+        document.getElementById('smefpw').innerHTML += "<br><span style=\"font-family: verdana, sans-serif;\"><span style=\"color: #009EBD;\">[smef]</span> current site " + currentLocation + " differs from official site example.org<br></font>";
+    }
 }
+
 // site checker
